abf5434ef419c3b863c08d9c1cdd649a17863985
aben20807 <aben20807@gmail.com>
2021-04-18 16:47:02 +0800

    Split bus machine

--- hw4/src/bus_booking_system/bus_booking_machine.cc
+++ hw4/src/bus_booking_system/bus_booking_machine.cc
@@ -1,1 +1,25 @@
 #include "bus_booking_machine.h"
+
+/**
+ * @brief Adapter to make aggregate struct be shared.
+ *
+ * @tparam T The type of aggregate struct.
+ * @tparam Args The variadic type of args.
+ * @param args The in-order elements of aggregate struct.
+ * @return std::shared_ptr<T>
+ */
+template <typename T, typename... Args>
+static std::shared_ptr<T> make_aggregate_shared(Args &&... args) {
+  return std::make_shared<T>(T{std::forward<Args>(args)...});
+}
+
+void BusBookingMachine::MakeBooking(Passenger *const passenger,
+                                    BusForBooking *const bus,
+                                    const int num_of_people) {
+  auto booking = make_aggregate_shared<BusBooking>(
+      passenger->get_name(), bus->get_name(), num_of_people,
+      bus->get_departure_date());
+  passenger->AddBooking(booking_index_, booking);
+  bus->AddBooking(booking_index_, booking);
+  booking_index_++;
+}
--- hw4/src/bus_booking_system/bus_booking_machine.h
+++ hw4/src/bus_booking_system/bus_booking_machine.h
@@ -5,19 +5,6 @@
 #include "bus_for_booking.h"
 #include "passenger.h"
 
-/**
- * @brief Adapter to make aggregate struct be shared.
- *
- * @tparam T The type of aggregate struct.
- * @tparam Args The variadic type of args.
- * @param args The in-order elements of aggregate struct.
- * @return std::shared_ptr<T>
- */
-template <typename T, typename... Args>
-std::shared_ptr<T> make_aggregate_shared(Args &&... args) {
-  return std::make_shared<T>(T{std::forward<Args>(args)...});
-}
-
 class BusBookingMachine {
  public:
   /**
@@ -50,14 +37,7 @@ class BusBookingMachine {
    * action.
    */
   void MakeBooking(Passenger *const passenger, BusForBooking *const bus,
-                   const int num_of_people) {
-    auto booking = make_aggregate_shared<BusBooking>(
-        passenger->get_name(), bus->get_name(), num_of_people,
-        bus->get_departure_date());
-    passenger->AddBooking(booking_index_, booking);
-    bus->AddBooking(booking_index_, booking);
-    booking_index_++;
-  }
+                   const int num_of_people);
 
  private:
   BusBookingMachine() {}
